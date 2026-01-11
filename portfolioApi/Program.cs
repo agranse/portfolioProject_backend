4e4261de3a882fef9f44b07aeba48c52e901d733
Adelaide Granse <grans036@umn.edu>
2026-01-10 20:33:16 -0600

    initialzing

--- portfolioApi/Program.cs
+++ portfolioApi/Program.cs
@@ -1,9 +1,30 @@
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Bind to Render's PORT
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(int.Parse(port));
 });
+
+// Add services (example)
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Build the app
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+// Run the app
+app.Run();

1cf3d1c4df09dd3d34bba67b61f5d46e5dd81575
Adelaide Granse <grans036@umn.edu>
2026-01-10 20:25:36 -0600

    initialzing

--- portfolioApi/obj/Release/net8.0/portfolioApi.AssemblyInfo.cs
+++ portfolioApi/obj/Release/net8.0/portfolioApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e15e21e5a6b75bb7ffc95c941c155ae5f4c9dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9186c0e9d23f3f95c235404e114a0bbdde0c9d59")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
